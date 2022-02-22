2b2e59bc6700faecb0d74585d64e3575d404a79d
SuperHyperHedgey <98873895+SuperHyperHedgey@users.noreply.github.com>
2022-02-21 16:56:21 -0800

    Yeah
    I did it

--- Calculator for Bird Part 1/Program.cs
+++ Calculator for Bird Part 1/Program.cs
@@ -4,63 +4,73 @@ namespace Calculator_for_Bird_Part_1
 {
     class Program
     {
+        static void OperatorSwitch(string opo, int realNum1, int realNum2)
+        {
+            switch (opo)
+            {
+                case "+":
+                    Console.WriteLine(realNum1 + realNum2);
+                    Success();
+                    break;
+
+                case "-":
+                    Console.WriteLine(realNum1 - realNum2);
+                    Success();
+                    break;
+
+                case "/":
+                    Console.WriteLine(realNum1 / realNum2);
+                    Success();
+                    break;
+
+                case "*":
+                    Console.WriteLine(realNum1 * realNum2);
+                    Success();
+                    break;
+
+                case "^":
+                    Console.WriteLine(realNum1 ^ realNum2);
+                    Success();
+                    break;
+
+                case "%":
+                    Console.WriteLine(realNum1 % realNum2);
+                    Success();
+                    break;
+
+                default:
+                    Console.WriteLine("Operator is not a valid operator");
+                    break;
+            }
+            static void Success()
+            {
+                Console.WriteLine("Mathematics!");
+                Console.WriteLine("With this we can improve the pipeline!");
+            }
+        }
         static void Main(string[] args)
         {
             try { 
             
 
-            Console.WriteLine("You stumbled across a calculator in a cave. A message is on the top. ");
-            Console.WriteLine("Read? \nPress the action button to continue.");
-            Console.ReadLine();
-            Console.WriteLine("\"Chris, enter the numbers\"");
+                Console.WriteLine("You stumbled across a calculator in a cave. A message is on the top. ");
+                Console.WriteLine("Read? \nPress the action button to continue.");
+                Console.ReadLine();
+                
+                Console.WriteLine("\"Chris, enter the numbers\"");
+                
+                Console.Write("Gimme the number, bird: ");
+                string num1 = Console.ReadLine();
+                int realNum1 = Convert.ToInt32(num1);          
 
-            Console.Write("Gimme the number, bird: ");
-            string num1 = Console.ReadLine();
+                Console.Write("Gimme an operator, tweeter: ");
+                string opo = Console.ReadLine();
 
-            int realNum1 = Convert.ToInt32(num1);          
+                Console.Write("Gimme the 2nd number, birdie: ");
+                string num2 = Console.ReadLine();
+                int realNum2 = Convert.ToInt32(num2);
 
-            Console.Write("Gimme an operator, tweeter: ");
-            string opo = Console.ReadLine();
-
-            Console.Write("Gimme the 2nd number, birdie: ");
-            string num2 = Console.ReadLine();
-            int realNum2 = Convert.ToInt32(num2);
-
-            OperatorSwitch(opo, realNum1, realNum2);
-
-            
-         
-
-                static void OperatorSwitch(string opo, int realNum1, int realNum2)
-                {
-                    switch (opo)
-                    {
-                        case "+":
-                            Console.WriteLine(realNum1 + realNum2);
-                            Success();
-                            break;
-                        case "-":
-                            Console.WriteLine(realNum1 - realNum2);
-                            Success();
-                            break;
-                        case "/":
-                            Console.WriteLine(realNum1 / realNum2);
-                            Success();
-                            break;
-                        case "*":
-                            Console.WriteLine(realNum1 * realNum2);
-                            Success();
-                            break;
-                        case "^":
-                            Console.WriteLine(realNum1 ^ realNum2);
-                            Success();
-                            break;
-                        case "%":
-                            Console.WriteLine(realNum1 % realNum2);
-                            Success();
-                            break;
-                    }
-                }
+                OperatorSwitch(opo, realNum1, realNum2);                                           
             }
 
             catch (DivideByZeroException e)
@@ -80,54 +90,11 @@ namespace Calculator_for_Bird_Part_1
             {
                 Console.WriteLine(e.Message);
             }
-            
-
-           /* if (opo == "+")
+            catch (Exception e)
             {
-                Console.WriteLine(realNum1 + realNum2);
-                Success();
+                Console.WriteLine(e.Message);
             }
-            else if (opo == "-")
-            {
-                Console.WriteLine(realNum1 - realNum2);
-                Success();
-            }
-
-            else if (opo == "/")
-            {
-                Console.WriteLine(realNum1 / realNum2);
-                Success();
-            }
-            else if (opo == "*")
-            {
-                Console.WriteLine(realNum1 * realNum2);
-                Success();
-            }
-            else if (opo == "^")
-            {
-                Console.WriteLine(realNum1^realNum2);
-                Success();
-            }
-            else if (opo == "%")
-            {
-                Console.WriteLine(realNum1 % realNum2);
-                Success();
-            } 
-            
-            
-            else
-            {
-                Console.WriteLine("Operator is not a valid operator, Mario");
-            }*/
-
-
-            static void Success()
-        {
-                Console.WriteLine("Mathematics!");
-                Console.WriteLine("With this we can improve the pipeline!");
-            }
-
-            
+           
         }
     }
 }
